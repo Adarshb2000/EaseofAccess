2401c12c384dc4c1e425a76e8f4090737dbaaf74
Adarsh <adarshb@Darsh.PC>
2020-10-18 15:51:02 +0530

    yoo

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
+from jnius import autoclass
+
+PythonActivity = autoclass('org.renpy.android.PythonActivity')
+activity = PythonActivity.mActivity
+
+Context = autoclass('android.content.Context')
+vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE)
 
 class EaseofAccess(MDApp):
     def build(self):
-        return MDLabel(text="Hello World")
+        vibrator.vibrate(10000)   
 
 if __name__ == "__main__":
     app = EaseofAccess()
